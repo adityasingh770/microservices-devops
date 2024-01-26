aa6283f4af94ae9fd9e6a05c35531544ee9adc46
aditya <adityasingh770@protonmail.com>
2024-01-27 01:39:34 +0530

    script: added sql script to initialize DB     script adds 'auth' DB and 'user' table with a test client

---
+++ src/auth/init.sql
@@ -0,0 +1,15 @@
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth@123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user (email, password) VALUES ('adityasngh770@gmail.com', 'Admin123');
