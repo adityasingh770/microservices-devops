d4452f27492cb549ddf925cb7c2bb86ac0bf3ffc
vivek <vivekchaurasia260@protonmail.com>
2024-02-09 07:38:58 +0530

    feat: added mongo db config and methods

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -2,19 +2,27 @@ import os
 import gridfs
 import pika
 import json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
-
+from bson.objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
-mongo = PyMongo(server)
+mongo_videos = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/videos"
+)
 
-fs = gridfs.GridFS(mongo.db)
+mongo_mp3 = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/mp3s"
+)
+
+fs_videos = gridfs.GridFS(mongo_videos.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
@@ -34,26 +42,49 @@ def login():
 def upload():
     access, err = validate.token(request)
 
+    if err:
+        return err
+
     access = json.loads(access)
 
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
-        
+
         for _, f in request.files.items():
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, fs_videos, channel, access)
 
             if err:
                 return err
-            
+
         return "success!", 200
     else:
         return "not authorized", 401
-    
+
 
 @server.route('/download', methods=['GET'])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f'{fid_string}.mp3')
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+
+    return "not authorized", 401
 
 
 if __name__ == "__main__":
